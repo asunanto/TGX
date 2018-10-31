844bfe09e904d3fc5bda3aa65e166ac17afe77b2
asunanto <wibowo.sutanto91@gmail.com>
2018-10-31 16:27:33 +1000

    added comments functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_31_032347) do
+ActiveRecord::Schema.define(version: 2018_10_31_034130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.boolean "flagged"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "product_name"
