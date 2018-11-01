c6614bb7039ccae11303562931fd2637e50ad173
asunanto <wibowo.sutanto91@gmail.com>
2018-11-01 22:59:23 +1000

    added initial roles for users

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
     belongs_to :user
     has_one_attached :image
     def can_change?(user)
-        self.user == user #|| user.has_role?(:admin)
+        self.user == user || user.has_role?(:admin)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   rolify
+  
+  after_create :assign_default_role
+  def assign_default_role
+    self.add_role(:basic) if self.roles.blank?
+  end
+  
   has_many :posts
   has_many :comments
   # Include default devise modules. Others available are:
