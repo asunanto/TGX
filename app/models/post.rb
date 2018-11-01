c07835f3752163ae56fa3e2a650dfb0640cdae8e
mitchell heiser <mitchell@mitchells-MacBook-Pro.local>
2018-11-01 15:20:09 +1000

    fixing merges'

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
     has_many :comments
     belongs_to :user
     has_one_attached :image
+    resourcify
     def can_change?(user)
-        self.user == user 
+        self.user == user || user.has_role?(:admin)
     end
 end
