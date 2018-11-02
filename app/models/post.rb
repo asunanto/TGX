ac94c0104c4cfbdc5380a26271b33a1d2b10c7f9
thanhthedev <huythanhdao@gmail.com>
2018-11-02 14:00:07 +1000

    validate posts and rounded buy prices to 2 decimals

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ApplicationRecord
     has_many :comments
     belongs_to :user
     has_one_attached :image
+
+
+    validates :product_name, :price, :location, :description, :presence => true
   
     def price_in_cents
-        (price * 100).to_i
+        (price * 100).to_i.round(2)
     end
     
     def can_change?(user)
